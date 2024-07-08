421ef34c4c9f7389d6fd85c9b3e8d2e0e7e69a69
Diksha <makkardiksha02@gmail.com>
2024-07-09 01:02:02 +0530

    Create ProductApplication.java

---
+++ src/main/java/com/example/product/ProductApplication.java
@@ -0,0 +1,27 @@
+package com.example.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer configure(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry reg){
+				reg.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
+
+}
